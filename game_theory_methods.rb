f9abe7296119898d784d043fac32bd724a4d4fbb
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-04-25 23:28:04 -0400

    added methods to generate 2d array and print array out in 2d display

--- game_theory_methods.rb
+++ game_theory_methods.rb
@@ -6,7 +6,23 @@ collection.each do |x|
   end
   puts ""
 end
-
 # this will print out a 100 by 100 grid on your command line
 # this is the core functionality that I am going to use to write some game theory
 # programs
+
+
+twodee = []
+for x in 1..100 do
+  twodee.push(Array.new(100,"O"))
+end
+# this will generate a 2d array and fill it with various values
+
+
+(0..99).to_a.each do |x|
+  (0..99).to_a.each do |y|
+    print twodee[x][y]
+  end
+  puts ""
+end
+# this will iterate over the array generated above and print out a board that
+# corresponds with those values
